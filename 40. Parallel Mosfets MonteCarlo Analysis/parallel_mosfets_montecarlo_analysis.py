2b9ab099c5698145251d57d4315bcea89d6839de
Guillaume Fontes <guillaume@simba.io>
2023-11-27 21:18:23 +0100

    add MonteCarlo parallel mosfets

--- 40. Parallel Mosfets MonteCarlo Analysis/parallel_mosfets_montecarlo_analysis.py
+++ 40. Parallel Mosfets MonteCarlo Analysis/parallel_mosfets_montecarlo_analysis.py
@@ -5,7 +5,6 @@ import multiprocessing
 from tqdm import tqdm
 from aesim.simba import ProjectRepository
 from datetime import datetime
-import matplotlib.pyplot as plt
 import pandas as pd
 
 
@@ -14,12 +13,11 @@ import pandas as pd
 #############################
 
 iterations = range(1000)
-param = {
-        'Rdson11' : {'nominal': 60e-3, 'tolerance': 0.1},
-        'Rdson12' : {'nominal': 60e-3, 'tolerance': 0.1},
-        'Rg11' : {'nominal': 20, 'tolerance': 0.1},
-        'Rg12' : {'nominal': 20, 'tolerance': 0.1},
-        }
+mosfet_index_list = ['11', '12', '13']
+param = dict()
+for mosfet_index in mosfet_index_list:
+    param['Rdson'+ mosfet_index] = {'nominal': 60e-3, 'tolerance': 0.1}
+    param['Rg'+ mosfet_index] = {'nominal': 20, 'tolerance': 0.1}
 
 
 #############################
@@ -44,34 +42,40 @@ def generate_random_values(param):
 def run_simulation(sim_number, manager_result_dict, lock):
     """
     Run SIMBA Simulation and place the results in "manager_result_dict"
-
-    :param: id_ref, d-axis current refereance[A]
-    :param: iq_ref, q-axis current refereance[A] 
     """   
     with lock:
         script_folder = os.path.realpath(os.path.dirname(__file__))
         file_path = os.path.join(script_folder, "parallel_mosfets_montecarlo_analysis.jsimba")
         project = ProjectRepository(file_path)
-    design = project.GetDesignByName('Design 1 - Cauer')
+    design = project.GetDesignByName('Design 2')
     param_values = generate_random_values(param)
-    design.Circuit.GetDeviceByName('T11').Ron = param_values['Rdson11']
-    design.Circuit.GetDeviceByName('T12').Ron = param_values['Rdson12']
-    design.Circuit.GetDeviceByName('T11').Rgon = param_values['Rg11']
-    design.Circuit.GetDeviceByName('T12').Rgon = param_values['Rg12']
-    design.Circuit.GetDeviceByName('T11').Rgoff = param_values['Rg11']
-    design.Circuit.GetDeviceByName('T12').Rgoff = param_values['Rg12']
+    for mosfet_index in mosfet_index_list:
+        design.Circuit.GetDeviceByName('T'+mosfet_index).Ron = param_values['Rdson'+mosfet_index]
+        design.Circuit.GetDeviceByName('T'+mosfet_index).Rgon = param_values['Rg'+mosfet_index]    
+        design.Circuit.GetDeviceByName('T'+mosfet_index).Rgoff = param_values['Rg'+mosfet_index]    
     job = design.TransientAnalysis.NewJob()
     status = job.Run()
     if str(status) != "OK": 
         print (job.Summary()[:-1])
+    
+    # manager_result_dict[sim_number] = []
+    # for mosfet_index in mosfet_index_list:
+    #     manager_result_dict[sim_number].append(param_values['Rdson'+mosfet_index])
+    #     manager_result_dict[sim_number].append(param_values['Rg'+mosfet_index])
+    #     manager_result_dict[sim_number].append(job.GetSignalByName('T'+mosfet_index +' - Average Total Losses (W)').DataPoints[-1])
+    #     manager_result_dict[sim_number].append(job.GetSignalByName('T'+mosfet_index +' - Junction Temperature (°)').DataPoints[-1])
     manager_result_dict[sim_number] = [param_values['Rdson11'],
-                                    param_values['Rdson12'],
-                                    param_values['Rg11'],
-                                    param_values['Rg12'],
-                                    job.GetSignalByName('T11 - Average Total Losses (W)').DataPoints[-1],
-                                    job.GetSignalByName('T12 - Average Total Losses (W)').DataPoints[-1],
-                                    job.GetSignalByName('T11 - Junction Temperature (°)').DataPoints[-1],
-                                    job.GetSignalByName('T12 - Junction Temperature (°)').DataPoints[-1]]
+                                       param_values['Rg11'],
+                                       job.GetSignalByName('T11 - Average Total Losses (W)').DataPoints[-1],
+                                       job.GetSignalByName('T11 - Junction Temperature (°)').DataPoints[-1],
+                                       param_values['Rdson12'],
+                                       param_values['Rg12'],
+                                       job.GetSignalByName('T12 - Average Total Losses (W)').DataPoints[-1],
+                                       job.GetSignalByName('T12 - Junction Temperature (°)').DataPoints[-1],
+                                       param_values['Rdson13'],
+                                       param_values['Rg13'],
+                                       job.GetSignalByName('T13 - Average Total Losses (W)').DataPoints[-1],
+                                       job.GetSignalByName('T13 - Junction Temperature (°)').DataPoints[-1]]
 
 def run_simulation_star(args):
     """
@@ -83,7 +87,7 @@ def run_simulation_star(args):
 #         MAIN SCRIPT       #
 #############################
 
-if __name__ == "__main__": # Called only in main thread. It confirms that the code is under main function
+if __name__ == "__main__": # Called only in main thread
 
     manager = multiprocessing.Manager()
     lock = manager.Lock()
@@ -94,7 +98,12 @@ if __name__ == "__main__": # Called only in main thread. It confirms that the co
         pass
     
     # Store results in dedicated dictionnary
-    res = {'Rdson11':[], 'Rdson12':[], 'Rg11':[], 'Rg12':[], 'Losses11':[], 'Losses12':[], 'Tj11':[], 'Tj12':[]}
+    res = dict()
+    for mosfet_index in mosfet_index_list:
+        res['Rdson'+mosfet_index] = []
+        res['Rg'+mosfet_index] = []
+        res['Losses'+mosfet_index] = []
+        res['Tj'+mosfet_index] = []
     for item in manager_result_dict.items():
         for key, var_position in zip(res.keys(), range(len(res))):
             res[key].append(item[1][var_position])
--- 40. Parallel Mosfets MonteCarlo Analysis/parallel_mosfets_montecarlo_plot.py
+++ 40. Parallel Mosfets MonteCarlo Analysis/parallel_mosfets_montecarlo_plot.py
@@ -1,4 +1,4 @@
-# Load modules
+#%% Load modules
 import os
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -11,61 +11,67 @@ def plot(df):
     # Plot figures 
     fig = plt.figure(figsize = (16, 9))
 
-    ax1 = fig.add_subplot(241)
+    ax1 = fig.add_subplot(331)
     ax1.plot(iterations, df['Losses11'], linestyle='', color='green', marker='o', markerfacecolor='blue')
     ax1.set_xlabel('iteration', fontsize = 9)
     ax1.set_ylabel('Losses', fontsize = 9)
     ax1.set_title("Losses T11", fontsize = 9)
 
-    ax2 = fig.add_subplot(242)
+    ax2 = fig.add_subplot(332)
     ax2.plot(iterations, df['Losses12'], linestyle='', color='red', marker='o', markerfacecolor='purple')
     ax2.set_xlabel("iteration", fontsize = 9)
     ax2.set_ylabel("Losses", fontsize = 9)
     ax2.set_title("Losses T12", fontsize = 9)
 
-    ax3 = fig.add_subplot(243)
-    ax3.plot(iterations, df['Tj11'], linestyle='', color='red', marker='o', markerfacecolor='purple')
-    ax3.set_xlabel("iteration", fontsize = 9)
-    ax3.set_ylabel("Tj", fontsize = 9)
-    ax3.set_title("Tj11", fontsize = 9)
+    ax2 = fig.add_subplot(333)
+    ax2.plot(iterations, df['Losses13'], linestyle='', color='red', marker='o', markerfacecolor='purple')
+    ax2.set_xlabel("iteration", fontsize = 9)
+    ax2.set_ylabel("Losses", fontsize = 9)
+    ax2.set_title("Losses T13", fontsize = 9)
 
-    ax4 = fig.add_subplot(244)
-    ax4.plot(iterations, df['Tj12'], linestyle='', color='red', marker='o', markerfacecolor='purple')
-    ax4.set_xlabel("iteration", fontsize = 9)
-    ax4.set_ylabel("Tj", fontsize = 9)
-    ax4.set_title("Tj12", fontsize = 9)
-
-    ax5 = fig.add_subplot(245)
+    ax5 = fig.add_subplot(334)
     ax5.plot(df['Rdson11'], df['Losses11'], linestyle='', color='red', marker='o', markerfacecolor='purple')
     ax5.set_xlabel("Rdson", fontsize = 9)
     ax5.set_ylabel("Losses", fontsize = 9)
     ax5.set_title("Losses T11 vs Rdson 11", fontsize = 9)
 
-    ax6 = fig.add_subplot(246)
+    ax6 = fig.add_subplot(335)
     ax6.plot(df['Rdson12'], df['Losses11'], linestyle='', color='red', marker='o', markerfacecolor='purple')
     ax6.set_xlabel("Rdson", fontsize = 9)
     ax6.set_ylabel("Losses", fontsize = 9)
-    ax6.set_title("Losses T12 vs Rdson 11", fontsize = 9)
+    ax6.set_title("Losses T11 vs Rdson 12", fontsize = 9)
 
-    ax7 = fig.add_subplot(247)
-    ax7.plot(df['Rdson12'], df['Losses11'], linestyle='', color='red', marker='o', markerfacecolor='purple')
+    ax7 = fig.add_subplot(336)
+    ax7.plot(df['Rdson13'], df['Losses11'], linestyle='', color='red', marker='o', markerfacecolor='purple')
     ax7.set_xlabel("Rdson", fontsize = 9)
     ax7.set_ylabel("Losses", fontsize = 9)
-    ax7.set_title("Losses T11 vs Rdson 12", fontsize = 9)
+    ax7.set_title("Losses T11 vs Rdson 13", fontsize = 9)
 
-    ax8 = fig.add_subplot(248)
-    ax8.plot(df['Rdson12'], df['Losses12'], linestyle='', color='red', marker='o', markerfacecolor='purple')
-    ax8.set_xlabel("Rdson", fontsize = 9)
-    ax8.set_ylabel("Losses", fontsize = 9)
-    ax8.set_title("Losses T12 vs Rdson 12", fontsize = 9)
+    ax5 = fig.add_subplot(337)
+    ax5.plot(df['Rdson11'], df['Losses12'], linestyle='', color='red', marker='o', markerfacecolor='purple')
+    ax5.set_xlabel("Rdson", fontsize = 9)
+    ax5.set_ylabel("Losses", fontsize = 9)
+    ax5.set_title("Losses T12 vs Rdson 11", fontsize = 9)
+
+    ax6 = fig.add_subplot(338)
+    ax6.plot(df['Rdson12'], df['Losses12'], linestyle='', color='red', marker='o', markerfacecolor='purple')
+    ax6.set_xlabel("Rdson", fontsize = 9)
+    ax6.set_ylabel("Losses", fontsize = 9)
+    ax6.set_title("Losses T12 vs Rdson 12", fontsize = 9)
+
+    ax7 = fig.add_subplot(339)
+    ax7.plot(df['Rdson13'], df['Losses12'], linestyle='', color='red', marker='o', markerfacecolor='purple')
+    ax7.set_xlabel("Rdson", fontsize = 9)
+    ax7.set_ylabel("Losses", fontsize = 9)
+    ax7.set_title("Losses T12 vs Rdson 13", fontsize = 9)
 
     fig.tight_layout(pad = 2)
     plt.show()
 
 # Load dataframe
 script_folder = os.path.realpath(os.path.dirname(__file__))
-filename = "montecarlo_parallel_mosfets_2023-11-01"
+filename = "montecarlo_parallel_mosfets_2023-11-13"
 df = pd.read_pickle(os.path.join(script_folder, filename + ".pkl" ))
 
-# Plot results from dataframe
+#%% Plot results from dataframe
 plot(df)
