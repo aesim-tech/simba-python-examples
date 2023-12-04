e2f50c40d696be939fcc3a36ff3084855e6e68de
Guillaume Fontes <guillaume@simba.io>
2023-12-04 20:57:08 +0100

    add simple exp

---
+++ 34. Optimization/max_power.py
@@ -0,0 +1,35 @@
+from aesim.simba import ProjectRepository
+import os
+import numpy as np
+from scipy.optimize import minimize
+
+
+# Objective function to maximize power
+def objective_function(R_load):
+    current_folder = os.path.dirname(os.path.abspath(__file__))
+    project = ProjectRepository(os.path.join(current_folder , "max_power.jsimba"))
+    design = project.GetDesignByName('Design 1')
+    design.Circuit.GetDeviceByName('R2').Value = R_load[0]
+    job = design.TransientAnalysis.NewJob()
+    status = job.Run()
+    Iout = job.GetSignalByName('R2 - Current').DataPoints
+    power = Iout[-1]**2*R_load
+    project.Save()
+    return -power  # Negative sign because we're maximizing
+
+
+# Initial guess for R_load
+initial_guess = np.array([3.4])
+
+# Bounds for R_load
+bounds = [(2.5, 3.5)]  # Load resistance should be between 2.5 and 3.5 ohms
+
+# Optimize using the L-BFGS-B method
+result = minimize(objective_function, initial_guess, method='L-BFGS-B', bounds=bounds)
+
+# Display the result
+optimal_R_load = result.x[0]
+max_power = -result.fun  # Convert back to positive power value
+
+print('Optimal R_load = {0:.4f} Ω'.format(optimal_R_load))
+print('Transferred Power P = {0:.4f} W'.format(max_power))
