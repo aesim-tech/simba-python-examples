2ad3b38ae4249f7d8b3d308e90fc1963871c294e
Guillaume Fontes <guillaume@simba.io>
2024-04-10 12:21:10 +0200

    wip

--- 44. Modulation Strategies Motor Drive/modulation_strategies_motor_drive.py
+++ 44. Modulation Strategies Motor Drive/modulation_strategies_motor_drive.py
@@ -1,114 +1,135 @@
 #%% Load modules
-from aesim.simba import ProjectRepository
-import matplotlib.pyplot as plt
-import os
+import os, multiprocessing
+from tqdm import tqdm
+from aesim.simba import ProjectRepository, License
 import numpy as np
+import pandas as pd
+from datetime import datetime
 
 
-# Get project
-script_folder = os.path.realpath(os.path.dirname(__file__))
-file_path = os.path.join(script_folder, "modulation_strategies_motor_drive.jsimba")
-project = ProjectRepository(file_path)
+#############################
+#         PARAMETERS        #
+#############################
+number_of_parallel_simulations = License.NumberOfAvailableParallelSimulationLicense() # Number of available Parallel Simulation License
+number_of_parallel_simulations = 2
 
-# Dictionary containing each design
-modulation_dict = {"SVPWM": project.GetDesignByName('SVPWM_modulation'),
-              "DPWM": project.GetDesignByName('DPWM_modulation'),
-              "SPWM": project.GetDesignByName('SPWM_modulation'),
-              }
-# Switching frequencies and Speed definition 
-switching_frequencies = [2e4, 4e4, 6e4]
-rpm_speeds = [2000, 4000]
+# Define two operating points
+rpm_speed_refs = {'op_point1': 2000, 'op_point2': 4000}
+torque_refs = {'op_point1': 250, 'op_point2': 20}
+id_refs = {'op_point1': -157.47, 'op_point2': -19.32}
+iq_refs = {'op_point1': 157.03, 'op_point2': 11.52}
 
-# Id, Iq & Torque definition for different Speeds
-Id_dict = {2000:-157.47, 4000:-19.32}
-Iq_dict = {2000:157.03, 4000:11.52}
-torque_dict = {2000:250, 4000:20}
+# Define modulation strategies and switching frequencies
+# modulations = ['SVPWM', 'DPWM', 'SPWM']
+# switching_frequencies = [2e4, 4e4, 6e4]
+modulations = ['SVPWM', 'DPWM']
+switching_frequencies = [2e4, 4e4]
 
-# X-axis Data values/class
-loss_types = ["MOS Conduction", "MOS Switching", "PMSM Copper", "PMSM Iron"]
+if os.environ.get("SIMBA_SCRIPT_TEST"): # To accelerate unit tests
+    pass
 
-# Graph construction
-x_bars = np.arange(len(loss_types))  # the label locations
-num_bars = len(switching_frequencies)
-width = 1.0/(num_bars+1)  # the width of the bars
-"""
-the centers of the bars are at positions 0, 1, ..., N-1
-the distance between two bars: 1-bar_width
-the first bar begins at: 0-bar_width/2
-to have an equal spacing between the bar and the left margin as between the bars themselves:
-    - the left margin can be set at: 0-bar_width/2-(1-bar_width),
-    - similarly, the right margin can be set to: N-1+bar_width/2+(1-bar_width)
-"""
-left_margin = 0 - width/2 - (1-width)
-right_margin = len(loss_types) - 1 + width/2 + (1-width)
+#############################
+#           METHODS         #
+#############################
 
-fig, axs = plt.subplots(nrows=len(modulation_dict), ncols=len(rpm_speeds), layout='constrained')
-fig.set_size_inches(16, 9)
+def run_simulation(torque_ref, id_ref, iq_ref, rpm_speed_ref, fpwm, modulation_key, sim_number, manager_result_dict, lock):
+    """
+    Run SIMBA Simulation and place the results in "manager_result_dict"
+    """ 
+    log = False # if true, log simulation results
 
-# Sweep of operating points for each design    
-for mod_item_index, (keyname, design) in enumerate(modulation_dict.items()):   # modulation strategy loop
+    # Get simba project
+    with lock:
+        script_folder = os.path.realpath(os.path.dirname(__file__))
+        file_path = os.path.join(script_folder, "modulation_strategies_motor_drive.jsimba")
+        project = ProjectRepository(file_path)
+    
+    # Get design according modulation strategy
+    design = project.GetDesignByName(modulation_key+'_modulation')
+
+    # Set JMAG motor model reference
     motor = design.Circuit.GetDeviceByName('JmagRTMotor')
     motor.RttFilePath.UserValue = os.path.join(script_folder, 'withHF_4000rpm20Nm.rtt')
+    
+    # Set speeds and id, iq references
+    speed_source_rads = design.Circuit.GetDeviceByName('W1')
+    speed_source_rads.Voltage = (rpm_speed_ref*np.pi/30)
+    id_ref_block = design.Circuit.GetDeviceByName('Id_ref')
+    id_ref_block.Value = id_ref
+    iq_ref_block = design.Circuit.GetDeviceByName('Iq_ref')
+    iq_ref_block.Value = iq_ref
+    carrier = design.Circuit.GetDeviceByName('Carrier')
+    carrier.Frequency = fpwm
 
-    print('***************** speed', rpm_speeds, 'circuit:', keyname)
+    # Run calculation
+    job = design.TransientAnalysis.NewJob()
+    status = job.Run()
+    if str(status) != "OK" or log: 
+        print (job.Summary()[:-1])
 
-    for rpm_index, rpm in enumerate(rpm_speeds):   # rpm speed loop
-        
-        # Set rpm value
-        speed_source_rads = design.Circuit.GetDeviceByName('W1')
-        speed_source_rads.Voltage = (rpm*np.pi/30)
+    # Get inverter Losses
+    conduction_losses = sum([job.GetSignalByName('T' + str(n) + ' - Average Conduction Losses (W)').DataPoints[-1] for n in range(1, 7)])
+    switching_losses = sum([job.GetSignalByName('T' + str(n) + ' - Average Switching Losses (W)').DataPoints[-1] for n in range(1, 7)])
+                                
+    # Get motor losses
+    copper_losses = job.GetSignalByName('JmagRTMotor - Copper Loss (average)').DataPoints[-1] 
+    iron_losses = job.GetSignalByName('JmagRTMotor - Iron Loss (average)').DataPoints[-1] 
+    if log:
+        print(f"Check speed = {rpm_speed_ref} rpm for {modulation_key} - {round(fpwm/1000)} kHz", conduction_losses, switching_losses, copper_losses, iron_losses)
 
-        # Id & Iq are set for each Speed swept
-        Id_ref = design.Circuit.GetDeviceByName('Id_ref')
-        Id_ref.Value = Id_dict[rpm]
-        Iq_ref = design.Circuit.GetDeviceByName('Iq_ref')
-        Iq_ref.Value = Iq_dict[rpm]
+    manager_result_dict[sim_number] = [rpm_speed_ref, torque_ref, modulation_key, fpwm, conduction_losses, switching_losses, copper_losses, iron_losses]
 
-        # color constrast for graph construction
-        green_color = 0.8   # initial bar color
-        multiplier = 0      # shift x position
-        ymax = 0            # Y-axis data values
+def run_simulation_star(args):
+    """
+    Helper function used to call run_simulation with a single argument
+    """
+    return run_simulation(*args)
 
-        Carrier = design.Circuit.GetDeviceByName('Carrier')
-        for fpwm in switching_frequencies:   # fpwm loop
-            
-            # Set fpwm value
-            Carrier.Frequency = fpwm
-            
-            # Run calculation
-            job = design.TransientAnalysis.NewJob()
-            status = job.Run()
-            if str(status) != "OK": 
-                print (job.Summary()[:-1])
+#############################
+#         MAIN SCRIPT       #
+#############################
 
-            # Inverter Losses
-            conduction_losses = sum([job.GetSignalByName('T' + str(n) + ' - Average Conduction Losses (W)').DataPoints[-1] for n in range(1, 7)])
-            switching_losses = sum([job.GetSignalByName('T' + str(n) + ' - Average Switching Losses (W)').DataPoints[-1] for n in range(1, 7)])
-                                        
-            # Motor losses
-            copper_losses = job.GetSignalByName('JmagRTMotor - Copper Loss (average)').DataPoints[-1] 
-            iron_losses = job.GetSignalByName('JmagRTMotor - Iron Loss (average)').DataPoints[-1] 
-            print(f"Check speed = {rpm} rpm for circuit <{keyname}> - {round(fpwm/1000)} kHz", conduction_losses, switching_losses, copper_losses, iron_losses)
+# Distribute and run the calculations. Results are saved in result_dict
+if __name__ == "__main__": # Called only in main thread. It confirms that the code is under main function
 
-            # Plot Curve
-            loss_results = [conduction_losses, switching_losses, copper_losses, iron_losses]
-            ymax_ = np.max(loss_results)
-            ymax = np.max((ymax_, ymax))
-            offset = width * multiplier
-            bar_plot = axs[mod_item_index, rpm_index].bar(x_bars + offset, loss_results, width, label=f"{round(fpwm/1000)}KHz", align='center', color=(0, green_color , 0))
-            axs[mod_item_index, rpm_index].bar_label(bar_plot, padding=3, fontsize=5)
-            multiplier += 1
-            green_color = green_color - 0.3
+    manager = multiprocessing.Manager()
+    lock = manager.Lock()
+    manager_result_dict = manager.dict()
 
-        # Add some text for labels, title and custom x-axis tick labels, etc.
-        axs[mod_item_index, rpm_index].set_xlabel('Loss Types')
-        axs[mod_item_index, rpm_index].set_ylabel('Losses (W)')
-        axs[mod_item_index, rpm_index].set_title(f'Loss comparison at T = {torque_dict[rpm]} N.m & N = {rpm} rpm for {keyname}')
-        axs[mod_item_index, rpm_index].set_xticks(x_bars + width*(num_bars-1)/2, loss_types)
-        axs[mod_item_index, rpm_index].legend(loc='upper right', ncols=num_bars)
-        axs[mod_item_index, rpm_index].set_ylim(0, ymax*1.25)
-        axs[mod_item_index, rpm_index].grid(visible=True, which="major", axis='y')
+    # Create all scenarii
+    pool_args = []
+    sim_nb = 0
+    #for op_key in ['op_point1', 'op_point2']:
+    for op_key in ['op_point1']:
+        for mod_key in modulations:
+            for fpwm in switching_frequencies:
+                pool_args.append((torque_refs[op_key], id_refs[op_key], iq_refs[op_key], rpm_speed_refs[op_key], fpwm, mod_key, sim_nb, manager_result_dict, lock))
+                sim_nb += 1
+    
+    # Start process pool
+    pool = multiprocessing.Pool(number_of_parallel_simulations)
+    for _ in tqdm(pool.imap(run_simulation_star, pool_args), total=len(pool_args)):
+        pass
 
-fig.tight_layout()
+    # Store results in dedicated dictionnary
+    res = dict()
+    res['speed'] = []
+    res['torque'] = []
+    res['modulation'] = []
+    res['fpwm'] = []
+    res['conduction'] = []
+    res['switching'] = []
+    res['copper'] = []
+    res['iron'] = []
 
-plt.show()
+    for item in manager_result_dict.items():
+        for key, var_position in zip(res.keys(), range(len(res))):
+            res[key].append(item[1][var_position])
+
+    # Store results in dataframe to write it in a file
+    df = pd.DataFrame(res)
+    script_folder = os.path.realpath(os.path.dirname(__file__))
+    filename = "mod_strat_motor_drive_" + datetime.now().strftime("%Y-%m-%d")
+    df.to_pickle(os.path.join(script_folder, filename + ".pkl"))
+
+    
