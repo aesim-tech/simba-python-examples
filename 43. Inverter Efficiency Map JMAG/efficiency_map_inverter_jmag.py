0c257a3148f04bef2df17353f564e4eeb1cb014d
manu <manu@aesim.tech>
2025-09-23 13:31:16 -0700

    Example 43. Added adaptative current controller.

--- 43. Inverter Efficiency Map JMAG/efficiency_map_inverter_jmag.py
+++ 43. Inverter Efficiency Map JMAG/efficiency_map_inverter_jmag.py
@@ -1,131 +1,127 @@
 """
-Customization of efficiency map script available in python examples
+Efficiency Map
 """
 
-import numpy, multiprocessing, os
+import numpy
+import threading
+import os
 import math
-from tqdm import tqdm
+import tqdm  # for the progress bar
 from aesim.simba import ProjectRepository, License
-
 
 #############################
 #   SIMULATION PARAMETERS   #
 #############################
-number_of_parallel_simulations = License.NumberOfAvailableParallelSimulationLicense() # Number of available parallel simulation license
+number_of_parallel_simulations = License.NumberOfAvailableParallelSimulationLicense()  # SIMBA parallel licenses
 case_temperature = 80           # Case temperature [Celsius]
-Rg = 4.5                         # Gate resistance [Ohm]
-switching_frequency = 50000;     # Switching Frequency [Hz]
-bus_voltage = 500.0;            # Bus Voltage [V]
+Rg = 4.5                        # Gate resistance [Ohm]
+switching_frequency = 50000     # Switching Frequency [Hz]
+bus_voltage = 500.0            # Bus Voltage [V]
 max_speed_ref = 4000            # RPM
-max_current_ref = 100.0          # A
+max_current_ref = 100.0         # A
 
-number_of_speed_points = 15    # Total number of simulations is number_of_speed_points * number_of_current_points (we can put 2 for faster results but less accurate)
-number_of_current_points = 15   # Total number of simulations is number_of_speed_points * number_of_current_points (we can put 2 for faster results but less accurate)
+number_of_speed_points = 15     # Total sims = speed_points * current_points
+number_of_current_points = 15   # Total sims = speed_points * current_points
 relative_minimum_speed = 0.2    # fraction of max_speed_ref
-relative_minimum_current = 0.2  # fraction of max_torque_ref
+relative_minimum_current = 0.2  # fraction of max_current_ref
 
 # Get motor parameters from Simba file
 current_folder = os.path.dirname(os.path.abspath(__file__))
-project = ProjectRepository(os.path.join(current_folder , "efficiency_map_inverter_jmag.jsimba"))
+project = ProjectRepository(os.path.join(current_folder, "efficiency_map_inverter_jmag.jsimba"))
 simba_full_design = project.GetDesignByName('Design')
 Ld_H = float(simba_full_design.Circuit.GetVariableValue("Ld"))
 Lq_H = float(simba_full_design.Circuit.GetVariableValue("Lq"))
 PM_Wb = float(simba_full_design.Circuit.GetVariableValue("Phi_mag"))
 NPP = float(simba_full_design.Circuit.GetVariableValue("NPP"))
 
-if os.environ.get("SIMBA_SCRIPT_TEST"): # Accelerate simulation in test environment.
-    number_of_speed_points = 2 
+if os.environ.get("SIMBA_SCRIPT_TEST"):  # Accelerate simulation in test environment.
+    number_of_speed_points = 2
     number_of_current_points = 2
+
+# Threading primitives
+semaphore = threading.Semaphore(number_of_parallel_simulations)  # throttle concurrent jobs
+file_lock = threading.Lock()  # guard reading the .jsimba repository
 
 #############################
 #           METHODS         #
 #############################
-def run_simulation(id_ref, iq_ref, speed_ref, case_temperature, Rg, sim_number, result_dict, lock):
+def run_simulation(id_ref, iq_ref, speed_ref, case_temperature, Rg, sim_number, results):
     """
-    Run SIMBA Simulation of the design "Full Design" and place the results in "result_dict"
+    Run SIMBA Simulation of the design "Full Design" and place the results into 'results[sim_number]'.
 
-    :param: id_ref, d-axis current refereance[A]
-    :param: iq_ref, q-axis current refereance[A]    
-    :param: speed_ref, Speed Reference [RPM]
-    :param: case_temperature, Case Temperature [Celsius]
-    :param: Rg, Gate Resistance [Ohm]
-    :param: sim_number, Simulation Number. Used for log purpose [N.m]
-    :param: result_dict, Thread safedictionnary used to store results [N.m]
-    :param: lock, Mutex. Used to avoid race conditions.
+    :param id_ref: d-axis current reference [A]
+    :param iq_ref: q-axis current reference [A]
+    :param speed_ref: Speed Reference [RPM]
+    :param case_temperature: Case Temperature [Celsius]
+    :param Rg: Gate Resistance [Ohm]
+    :param sim_number: Simulation Number. Used as results index
+    :param results: shared list (index-safe) used to store results
     """
+    log = False  # if true, log simulation results
 
-    log = False # if true, log simulation results
+    # Respect SIMBA license concurrency
+    with semaphore:
+        # Read the jsimba file (guarded to avoid any race on repository load)
+        with file_lock:
+            local_project = ProjectRepository(os.path.join(current_folder, "efficiency_map_inverter_jmag.jsimba"))
 
-    # Read the jsimba file
-    with lock:
-        current_folder = os.path.dirname(os.path.abspath(__file__))
-        project = ProjectRepository(os.path.join(current_folder , "efficiency_map_inverter_jmag.jsimba"))
-    
-    simba_full_design = project.GetDesignByName('Design')
+        simba_full_design = local_project.GetDesignByName('Design')
 
-    # Set Test Target Data
-    # operating point
-    simba_full_design.Circuit.SetVariableValue("RPM", str(speed_ref))
-    simba_full_design.Circuit.GetDeviceByName("Id_ref").Value = str(id_ref)
-    simba_full_design.Circuit.GetDeviceByName("Iq_ref").Value = str(iq_ref)
-    
-    # inverter settings
-    simba_full_design.Circuit.SetVariableValue("Tcase", str(case_temperature))
-    simba_full_design.Circuit.SetVariableValue("fpwm", str(switching_frequency))
-    simba_full_design.Circuit.SetVariableValue("DC", str(bus_voltage))
+        # Set Test Target Data
+        # operating point
+        simba_full_design.Circuit.SetVariableValue("RPM", str(speed_ref))
+        simba_full_design.Circuit.GetDeviceByName("Id_ref").Value = str(id_ref)
+        simba_full_design.Circuit.GetDeviceByName("Iq_ref").Value = str(iq_ref)
 
-    for i in range(1, 6):
-        simba_full_design.Circuit.GetDeviceByName("T{0}".format(i)).Rgon = Rg
-        
-    if log: print ("\n{0}> Running Full Model... (Id_ref={1:.2f} A Iq_ref={2:.2f} A speed_ref={3:.2f} RPM)".format(sim_number, id_ref, iq_ref, speed_ref))
+        # inverter settings
+        simba_full_design.Circuit.SetVariableValue("Tcase", str(case_temperature))
+        simba_full_design.Circuit.SetVariableValue("fpwm", str(switching_frequency))
+        simba_full_design.Circuit.SetVariableValue("DC", str(bus_voltage))
 
-    # Run Simulation
-    job = simba_full_design.TransientAnalysis.NewJob()
-    status = job.Run()
-    
-    if str(status) != "OK": 
-        print (job.Summary()[:-1])
-        return; # ERROR 
-    if log: print (job.Summary()[:-1])
+        for i in range(1, 6):
+            simba_full_design.Circuit.GetDeviceByName(f"T{i}").Rgon = Rg
 
-    # Read and return results
-    inverter_losses = job.GetSignalByName('Inverter_Losses - Heat Flow').DataPoints[-1]
-    motor_losses = job.GetSignalByName('JmagRTMotor1 - Total Loss (average)').DataPoints[-1]
-    actual_torque = job.GetSignalByName('JmagRTMotor1 - Te').DataPoints[-1]
-    actual_speed_rpm = job.GetSignalByName('speed_rpm - Out').DataPoints[-1]
-    input_power = job.GetSignalByName('Pin - P').DataPoints[-1]
-    output_power= job.GetSignalByName('Pout - Out').DataPoints[-1]
+        if log:
+            print(f"\n{sim_number}> Running Full Model... (Id_ref={id_ref:.2f} A "
+                  f"Iq_ref={iq_ref:.2f} A speed_ref={speed_ref:.2f} RPM)")
 
-    if (actual_speed_rpm < 0): return; # ERROR 
+        # Run Simulation
+        job = simba_full_design.TransientAnalysis.NewJob()
+        status = job.Run()
 
-    if log: print ('{0}> Inverter Losses = {1:.2f}W'.format(sim_number, inverter_losses))
-    if log: print ('{0}> Motor Losses = {1:.2f}W'.format(sim_number, motor_losses))
-    if log: print ('{0}> Input Power = {1:.2f}W'.format(sim_number, input_power))
-    if log: print ('{0}> Actual Torque = {1:.2f}N.m Id Ref = {2:.2f} A Iq Ref = {3:.2f} A'.format(sim_number, actual_torque, id_ref, iq_ref))
-    if log: print ('{0}> Actual Speed = {1:.2f}RPM Speed Ref = {2:.2f} RPM'.format(sim_number, actual_speed_rpm, speed_ref))
+        if str(status) != "OK":
+            print(job.Summary()[:-1])
+            return  # ERROR
+        if log:
+            print(job.Summary()[:-1])
 
-    total_losses = inverter_losses + motor_losses
-    efficiency = 1 - total_losses / (total_losses + input_power)
-    if log: print ('{0}> Efficiency = {1:.2f}%  Input Power {2:.2f}W total_losses  {3:.2f}W'.format(sim_number, 100*efficiency, input_power, total_losses))
-    result_dict[sim_number] = [inverter_losses, motor_losses, actual_torque, actual_speed_rpm, efficiency, input_power, output_power]
+        # Read and return results
+        inverter_losses = job.GetSignalByName('Inverter_Losses - Heat Flow').DataPoints[-1]
+        motor_losses = job.GetSignalByName('JmagRTMotor1 - Total Loss (average)').DataPoints[-1]
+        actual_torque = job.GetSignalByName('JmagRTMotor1 - Te').DataPoints[-1]
+        actual_speed_rpm = job.GetSignalByName('speed_rpm - Out').DataPoints[-1]
+        input_power = job.GetSignalByName('Pin - P').DataPoints[-1]
+        output_power = job.GetSignalByName('Pout - Out').DataPoints[-1]
 
-def run_simulation_star(args):
-    """
-    Helper function used to call run_simulation with a single argument
-    """
-    return run_simulation(*args)
+        if actual_speed_rpm < 0:
+            return  # ERROR
+
+        total_losses = inverter_losses + motor_losses
+        efficiency = 1 - total_losses / (total_losses + input_power)
+
+        if log:
+            print(f'{sim_number}> Efficiency = {100*efficiency:.2f}%  Input Power {input_power:.2f}W '
+                  f'total_losses  {total_losses:.2f}W')
+
+        # Store results at fixed index (thread-safe since each sim_number is unique)
+        results[sim_number] = [inverter_losses, motor_losses, actual_torque,
+                               actual_speed_rpm, efficiency, input_power, output_power]
+
 
 def SelectIdIq(ref_idiq, current_ref, speed_ref):
     """
-    Calculate Id and Iq references calculated with MTPA and flux weakening algorithm.
-    Source: S. Morimoto, Y. Takeda, T. Hirasa and K. Taniguchi, "Expansion of operating limits for permanent magnet motor by current vector control considering inverter capacity," in IEEE Transactions on Industry Applications, vol. 26, no. 5, pp. 866-871, Sept.-Oct. 1990, doi: 10.1109/28.60058.
-    Args:
-        ref_idiq ([float, float]): Used to store the Id&Iq references calculated by this function
-        current_ref (float): current reference
-        speed_ref (float): speed reference
-
-    Returns:
-        boolean: True if success, False is this point is not achieveble
+    Calculate Id and Iq references with MTPA and flux weakening.
+    Returns True if success, False if this point is not achievable.
     """
     ret = False
     Vdc_V = bus_voltage
@@ -134,56 +130,53 @@ def SelectIdIq(ref_idiq, current_ref, speed_ref):
 
     Vo_V = Vdc_V / 2.0
     speed_radpsec = speed_rpm / 60 * 2 * math.pi
-    		
+
     Beta_MTPA_rad = 0.0
     if abs(Ld_H - Lq_H) > 1.0e-8:
-        nume = -PM_Wb + math.sqrt(PM_Wb**2 + 8 * (Lq_H - Ld_H)**2  * Ia_A**2)
+        nume = -PM_Wb + math.sqrt(PM_Wb**2 + 8 * (Lq_H - Ld_H)**2 * Ia_A**2)
         deno = 4.0 * (Lq_H - Ld_H) * Ia_A
-        Beta_MTPA_rad = math.asin(nume / deno) 
-    
-    id_MTPA_A = -1.0 * Ia_A * math.sin(Beta_MTPA_rad) 
+        Beta_MTPA_rad = math.asin(nume / deno)
+
+    id_MTPA_A = -1.0 * Ia_A * math.sin(Beta_MTPA_rad)
     iq_MTPA_A = Ia_A * math.cos(Beta_MTPA_rad)
-    
-    Flux_Wb = math.sqrt(math.pow(PM_Wb + Ld_H*id_MTPA_A, 2) + math.pow(Lq_H * iq_MTPA_A, 2))
-    corner_speed_radpsec_mech = ( Vo_V / Flux_Wb) / NPP 
+
+    Flux_Wb = math.sqrt((PM_Wb + Ld_H * id_MTPA_A) ** 2 + (Lq_H * iq_MTPA_A) ** 2)
+    corner_speed_radpsec_mech = (Vo_V / Flux_Wb) / NPP
     speed_radpsec_elec = speed_radpsec * NPP
-        
+
     if speed_radpsec < corner_speed_radpsec_mech:
-    #% MTPA (Mode 1)
+        # MTPA (Mode 1)
         ref_idiq[0] = id_MTPA_A
         ref_idiq[1] = iq_MTPA_A
         ret = True
-    else :
-    #	% Flux weakening (Mode 2)
+    else:
+        # Flux weakening (Mode 2)
         iq_FW_A = iq_MTPA_A
-        for i in range(100):
-            iq_A_old = iq_FW_A;
-            if (math.pow(Vo_V / speed_radpsec_elec, 2) - math.pow(Lq_H*iq_FW_A,2) ) < 0.0:
+        for _ in range(100):
+            iq_A_old = iq_FW_A
+            if (Vo_V / speed_radpsec_elec) ** 2 - (Lq_H * iq_FW_A) ** 2 < 0.0:
                 continue
-            
-            id_FW_A = (-1.0*PM_Wb + math.sqrt(math.pow(Vo_V/speed_radpsec_elec, 2) - math.pow(Lq_H*iq_FW_A,2) ) )/Ld_H
-            if(Ia_A*Ia_A - id_FW_A*id_FW_A < 0.0):
+
+            id_FW_A = (-PM_Wb + math.sqrt((Vo_V / speed_radpsec_elec) ** 2 - (Lq_H * iq_FW_A) ** 2)) / Ld_H
+            if Ia_A * Ia_A - id_FW_A * id_FW_A < 0.0:
                 continue
-            iq_FW_A = math.sqrt(Ia_A*Ia_A - id_FW_A*id_FW_A)
-            if math.fabs(iq_FW_A -iq_A_old) < 1.0e-3 :
+            iq_FW_A = math.sqrt(Ia_A * Ia_A - id_FW_A * id_FW_A)
+            if math.fabs(iq_FW_A - iq_A_old) < 1.0e-3:
                 ref_idiq[0] = id_FW_A
-                ref_idiq[1] = iq_FW_A	
+                ref_idiq[1] = iq_FW_A
                 ret = True
                 break
     return ret
-            
+
+
 #############################
 #         MAIN SCRIPT       #
 #############################
+if __name__ == "__main__":  # Called only in main thread.
 
-# Distribute and run the calculations. Results are saved in result_dict
-if __name__ == "__main__": # Called only in main thread. It confirms that the code is under main function
-
-    # update rtt path
+    # 1) Update RTT path
     current_folder = os.path.dirname(os.path.abspath(__file__))
     path_RTT = os.path.join(current_folder, "100k_D_D_I-.rtt")
-
-    # update rtt file path
 
     project = ProjectRepository(os.path.join(current_folder, 'efficiency_map_inverter_jmag.jsimba'))
     design = project.GetDesignByName('Design')
@@ -191,64 +184,74 @@ if __name__ == "__main__": # Called only in main thread. It confirms that the co
     pmsm.RttFilePath.UserValue = path_RTT
     project.Save()
 
-    #initialization
-    min_speed_ref = relative_minimum_speed * max_speed_ref;
-    min_current_ref = relative_minimum_current * max_current_ref;
-    
-    speed_refs = numpy.arange(min_speed_ref, max_speed_ref, (max_speed_ref - min_speed_ref)/number_of_speed_points)
-    current_refs = numpy.arange(min_current_ref, max_current_ref, (max_current_ref - min_current_ref)/number_of_current_points)
-    
-    manager = multiprocessing.Manager()
-    result_dict = manager.dict()
-    i=0
-    jobs=[]
-    lock = manager.Lock()
-    pool_args = []
+    # 2) Initialization
+    min_speed_ref = relative_minimum_speed * max_speed_ref
+    min_current_ref = relative_minimum_current * max_current_ref
 
-    # Create the run_simulation(...) arguments for each scenario
+    speed_refs = numpy.arange(min_speed_ref, max_speed_ref,
+                              (max_speed_ref - min_speed_ref) / number_of_speed_points)
+    current_refs = numpy.arange(min_current_ref, max_current_ref,
+                                (max_current_ref - min_current_ref) / number_of_current_points)
+
+    # Build job list (pool_args) and pre-size results
+    pool_args = []
+    i = 0
     for current_ref in current_refs:
         for speed_ref in speed_refs:
-            
             ref_idiq = [0.0, 0.0]
-            ret = SelectIdIq(ref_idiq, current_ref, speed_ref)
-            
-            if ret == True:
-                pool_args.append((ref_idiq[0], ref_idiq[1], speed_ref, case_temperature, Rg,  i, result_dict, lock));
-                i=i+1
+            if SelectIdIq(ref_idiq, current_ref, speed_ref):
+                pool_args.append((ref_idiq[0], ref_idiq[1], speed_ref, case_temperature, Rg, i))
+                i += 1
 
-    # Create and start the processing pool
-    pool = multiprocessing.Pool(number_of_parallel_simulations)
-    for _ in tqdm(pool.imap(run_simulation_star, pool_args), total=len(pool_args)):
-        pass
+    results = [None] * len(pool_args)
 
-    # plot the efficency map
+    # 3) Create and start threads
+    threads = []
+    for args in pool_args:
+        # append shared 'results' at the end of args for this thread
+        t = threading.Thread(target=run_simulation, args=(*args, results))
+        threads.append(t)
+        t.start()
+
+    # 4) Wait for completion with progress bar
+    for t in tqdm.tqdm(threads, desc="Running simulations"):
+        t.join()
+
+    # 5) Collect and save results
     inverter_losses = []
     motor_losses = []
-    input_power= []
-    Pout=[]
-    t = []
-    s = []
-    e = []
-    for i in result_dict.items():
-        inverter_losses.append(i[1][0])
-        motor_losses.append(i[1][1])
-        t.append(i[1][2])
-        s.append(i[1][3])
-        e.append(i[1][4])
-        input_power.append(i[1][5])
-        Pout.append(i[1][6])
+    input_power = []
+    Pout = []
+    t_arr = []
+    s_arr = []
+    e_arr = []
+
+    for r in results:
+        if r is None:
+            continue  # skip failed runs
+        inverter_losses.append(r[0])
+        motor_losses.append(r[1])
+        t_arr.append(r[2])
+        s_arr.append(r[3])
+        e_arr.append(r[4])
+        input_power.append(r[5])
+        Pout.append(r[6])
+
     inverter_losses = numpy.array(inverter_losses)
     motor_losses = numpy.array(motor_losses)
     input_power = numpy.array(input_power)
-    Pout =  numpy.array(Pout)
-    t = numpy.array(t)
-    s = numpy.array(s)
-    e = numpy.array(e)
-    current_folder = os.path.dirname(os.path.abspath(__file__))
-    numpy.savetxt(os.path.join(current_folder, "results/inverter_losses.txt"), inverter_losses)
-    numpy.savetxt(os.path.join(current_folder,"results/motor_losses.txt"), motor_losses)
-    numpy.savetxt(os.path.join(current_folder,"results/input_power.txt"), input_power)
-    numpy.savetxt(os.path.join(current_folder,"results/torque.txt"), t)
-    numpy.savetxt(os.path.join(current_folder,"results/speed.txt"), s)
-    numpy.savetxt(os.path.join(current_folder,"results/efficiency.txt"), e)
-    numpy.savetxt(os.path.join(current_folder,"results/Pout.txt"), Pout)
+    Pout = numpy.array(Pout)
+    t_arr = numpy.array(t_arr)
+    s_arr = numpy.array(s_arr)
+    e_arr = numpy.array(e_arr)
+
+    results_dir = os.path.join(current_folder, "results")
+    os.makedirs(results_dir, exist_ok=True)
+
+    numpy.savetxt(os.path.join(results_dir, "inverter_losses.txt"), inverter_losses)
+    numpy.savetxt(os.path.join(results_dir, "motor_losses.txt"), motor_losses)
+    numpy.savetxt(os.path.join(results_dir, "input_power.txt"), input_power)
+    numpy.savetxt(os.path.join(results_dir, "torque.txt"), t_arr)
+    numpy.savetxt(os.path.join(results_dir, "speed.txt"), s_arr)
+    numpy.savetxt(os.path.join(results_dir, "efficiency.txt"), e_arr)
+    numpy.savetxt(os.path.join(results_dir, "Pout.txt"), Pout)
