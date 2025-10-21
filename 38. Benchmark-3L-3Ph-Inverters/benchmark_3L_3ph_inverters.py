affb3244c9211eebc5b1297af6cecdfa4009fa69
Guillaume Fontes <guillaume@simba.io>
2025-10-21 13:41:38 +0200

    new benchmark of 3-Level inverters

--- 38. Benchmark-3L-3Ph-Inverters/benchmark_3L_3ph_inverters.py
+++ 38. Benchmark-3L-3Ph-Inverters/benchmark_3L_3ph_inverters.py
@@ -1,84 +1,112 @@
-#%% Load modules
-import os
-from aesim.simba import ProjectRepository
-import numpy as np
+import os, multiprocessing
+from tqdm import tqdm
+from aesim.simba import ProjectRepository, License
 import pandas as pd
-import matplotlib.pyplot as plt
+from datetime import datetime
 
+#############################
+#         PARAMETERS        #
+#############################
+number_of_parallel_simulations = License.NumberOfAvailableParallelSimulationLicense() # Number of available Parallel Simulation License
 
-#%% Define topologies, switches and result dataframes
+# Define topologies, switches and result dataframes
 
-# topos = ['3L-NPC', '3L-T-type', '3L-FC']
-topos = ['3L-NPC', '3L-T-type']
+topos = ['3L-NPC', '3L-T-type', '3L-FC']
+#topos = ['3L-NPC', '3L-T-type']
 switches = dict()
 switches['3L-NPC'] = ['T1', 'T2', 'D1', 'D2', 'D5']
 switches['3L-T-type'] = ['T1', 'T2', 'T3', 'D1', 'D2', 'D3']
 switches['3L-FC'] = ['T1', 'T2', 'D1', 'D2']
 all_switches = list(set(switches['3L-NPC'] + switches['3L-T-type'] + switches['3L-FC']))
 
+if os.environ.get("SIMBA_SCRIPT_TEST"): # To accelerate unit tests
+    topos = ['3L-NPC']
 
-# Waveforms
-waveforms_df = pd.DataFrame([], columns=topos, index=['U12', 'iL1', 'iL2', 'iL3'])
-# Losses
-conduction_loss_df = pd.DataFrame([], columns=topos, index=all_switches)
-switching_loss_df = conduction_loss_df.copy(deep=True)
-Tj_df = conduction_loss_df.copy(deep=True)
+#############################
+#           METHODS         #
+#############################
 
-#%% Load simba project
-script_folder = os.path.realpath(os.path.dirname(__file__))
-file_path = os.path.join(script_folder, "benchmark_3L_3ph_inverters.jsimba")
-project = ProjectRepository(file_path)
+def run_simulation(topo, sim_number, manager_result_dict, lock):
+    """
+    Run SIMBA Simulation and place the results in "manager_result_dict"
+    """ 
+    log = False # if true, log simulation results
 
-#%% Run simulation for each topology and collect results
-for topo in topos:
+    # Get simba project
+    with lock:
+        script_folder = os.path.realpath(os.path.dirname(__file__))
+        file_path = os.path.join(script_folder, "benchmark_3L_3ph_inverters.jsimba")
+        project = ProjectRepository(file_path)
+
+    # Load design and run simulation
     design = project.GetDesignByName(topo)
     design.Circuit.SetVariableValue('ma', str(0.8))
-    design.TransientAnalysis.EndTime = 0.04
-    design.TransientAnalysis.TimeStep = 1e-6
+    design.TransientAnalysis.EndTime = 1.2
+    design.TransientAnalysis.TimeStep = 5e-8
     job = design.TransientAnalysis.NewJob()
     status = job.Run()
-    if str(status) != "OK": 
+    if str(status) != "OK" or log: 
         print (job.Summary())
-    for s in all_switches:
-        if s in switches[topo]:
-            conduction_loss_df.at[topo, s] = job.GetSignalByName(s + ' - Average Conduction Losses (W)').DataPoints[-1]
-            switching_loss_df.at[topo, s] = job.GetSignalByName(s + ' - Average Switching Losses (W)').DataPoints[-1]
-            # if topo == '3L-T-type' and s == 'T3':
-            #    print('debug T3 Tj')
-            Tj_df.at[topo, s] = job.GetSignalByName(s + ' - Junction Temperature (°)')
-        else:
-            conduction_loss_df.at[topo, s] = np.nan
-            switching_loss_df.at[topo, s] = np.nan
-            Tj_df.at[topo, s] = np.nan
-    waveforms_df.at[topo, 'U12'] = job.GetSignalByName('U12 - Voltage')
-    for k in range(1, 4):
-        waveforms_df.at[topo, 'iL'+str(k)] = job.GetSignalByName('L' + str(k) + ' - Current')
 
-#%% Plot results
-fig, axs = plt.subplots(nrows=2, ncols=1, layout='constrained')
-axs[0].plot(waveforms_df.loc['3L-NPC', 'U12'].TimePoints, waveforms_df.loc['3L-NPC','U12'].DataPoints, label='U12 (V)')
-for k in range(1, 4):
-    axs[1].plot(waveforms_df.loc['3L-NPC', 'iL'+str(k)].TimePoints, waveforms_df.loc['3L-NPC','iL'+str(k)].DataPoints, label='iL' + str(k))
-axs[1].set_xlabel('Time (s)')
-axs[0].set_ylabel('U12 (V)')
-axs[0].grid()
-axs[1].set_ylabel('Currents (A)')
-axs[1].grid()
-axs[1].legend()
-
-#%%
-n_rows = len(topos)
-hist_fig, axs = plt.subplots(nrows=n_rows, ncols=1, layout='constrained')
-hist_fig.set_size_inches(16, 16)
-
-axs
-
-for index in range(n_rows):
-    topo = topos[index]
-    ax = axs[index]
+    # Get and store waveforms, losses and Tj
+    waveforms = {}
+    conduction_loss = {}
+    switching_loss = {}
+    Tj = {}
+    waveforms['U12-data'] = job.GetSignalByName('U12 - Voltage').DataPoints
+    waveforms['U12-time'] = job.GetSignalByName('U12 - Voltage').TimePoints
     for s in switches[topo]:
-        ax.hist(conduction_loss_df.loc[topo, s].dropna(), bins=10, alpha=0.5, label=s)
-    ax.hist(switching_loss_df.loc[topo].dropna(), bins=10, alpha=0.5, label='Switching Losses (W)')
-    ax.set_title(f'Loss Distribution for {topo}')
-    ax.legend()
-#%%
+        conduction_loss[s] = job.GetSignalByName(s + ' - Average Conduction Losses (W)').DataPoints[-1]
+        switching_loss[s] = job.GetSignalByName(s + ' - Average Switching Losses (W)').DataPoints[-1]
+        Tj[s] = max(job.GetSignalByName(s + ' - Junction Temperature (°)').DataPoints)
+    
+    manager_result_dict[sim_number] = [topo, waveforms, conduction_loss, switching_loss, Tj]
+
+def run_simulation_star(args):
+    """
+    Helper function used to call run_simulation with a single argument
+    """
+    return run_simulation(*args)
+
+#############################
+#         MAIN SCRIPT       #
+#############################
+
+# Distribute and run the calculations. Results are saved in result_dict
+if __name__ == "__main__": # Called only in main thread. It confirms that the code is under main function
+
+    manager = multiprocessing.Manager()
+    lock = manager.Lock()
+    manager_result_dict = manager.dict()
+
+    # Create all scenarii
+    pool_args = []
+    sim_nb = 0
+    # for op_index in range(len(op_points)):
+    for topo in topos:
+        pool_args.append((topo, sim_nb, manager_result_dict, lock))
+        sim_nb += 1
+    
+    # Start process pool
+    pool = multiprocessing.Pool(number_of_parallel_simulations)
+    for _ in tqdm(pool.imap(run_simulation_star, pool_args), total=len(pool_args)):
+        pass
+
+# Store results in dedicated dictionnary
+    res = {}
+    res['topo'] = []
+    res['waveforms'] = []
+    res['conduction'] = []
+    res['switching'] = []
+    res['Tj'] = []
+
+    for item in manager_result_dict.items():
+        for key, var_position in zip(res.keys(), range(len(res))):
+            res[key].append(item[1][var_position])
+
+    # Store results in dataframe to write it in a file
+    df = pd.DataFrame(res)
+    script_folder = os.path.realpath(os.path.dirname(__file__))
+    filename = "benchmark_3L_" + datetime.now().strftime("%Y-%m-%d")
+    df.to_pickle(os.path.join(script_folder, filename + ".pkl"))
+    
---
+++ 38. Benchmark-3L-3Ph-Inverters/readme.md
@@ -0,0 +1,86 @@
+---
+tags:
+  - DC-AC
+  - Python
+  - Thermal
+---
+
+# Thermal benchmark of 3-Level 3-phase Voltage Source Inverters
+
+[Download **python script for simulation**](benchmark_3L_3ph_inverters.py)
+
+[Download **python script for plotting**](benchmark_3L_3ph_inverters_plot.py)
+
+[Download **Simba model**](benchmark_3L_3ph_inverters.jsimba)
+
+[Download **Python Library requirements**](requirements.txt)
+
+
+
+## Review of topology candidates
+
+This example proposes a python script which provides a thermal benchmark of three topologies of 3-Level 3-phase Voltage Source Inverters:
+
+* a Neutral Point Clamped (NPC),
+* a T-Type,
+* a Flying Capacitor (FC).
+
+The figures below show these different topologies.
+
+The requirements are:
+
+* DC voltage: 700 V,
+* Power: 40.9 kW,
+* Fundamental frequency: 50 Hz,
+* Apparent Switching frequency: 15 kHz.
+
+!!! note "note"
+    The *apparent* switching frequency is the switching frequency of the AC voltage waveform. It is twice the *real* switching frequency of devices for the FC topology and it is the *real* switching frequency of devices for the NPC and T-type topologies (but for these last ones, the powerswitches only operate during a half period of the fundamental!)
+
+### The NPC topology
+
+![npc](fig/npc.png)
+
+### The T-type topology
+
+![t-type](fig/t-type.png)
+
+### The FC topology
+
+![fc](fig/fc.png)
+
+All the topologies are compared with the same AC load and control which has been considered in open-loop.
+
+!!! info "info"
+    For the Flying Capacitor Topology, a Phase Disposition (PD) strategy - which allows a lower wTHD of the phase-phase voltage - has been implemented with a state machine in a C-code.
+
+
+## Thermal description
+
+Modules from Infineon module are considered:
+
+* the reference [F3L100R07W2H3_B11](datasheets/Infineon-F3L100R07W2H3_B11-DataSheet-v01_00-EN.pdf) for the NPC module,
+* the reference [F3L200R12W2H3_B11](datasheets/infineon-f3l200r12w2h3-b11-ds-ja.pdf) for the T-Type module,
+* the reference [IKZ75N65El5](datasheets/infineon-ikz75n65el5-datasheet-en.pdf) for the FC topology.
+
+The case temperature has been considered constant and has been set to 100 °C.
+
+
+## Results
+
+### Voltage Waveforms and Spectrum
+
+The figures below show the waveforms and the spectrum of the phase-phase voltage of the 3-Level topologies. The voltage waveform of a classic 2-Level inverter has also been plotted for comparison.
+
+![voltage waveforms](fig/voltage_waveforms.png)
+
+![voltage spectrum](fig/voltage_spectrum.png)
+
+As the apparent switching frequency of the 3-Level topologies is the same, the spectrum are identical. Yet, as previously mentionned, the *real* swicthing frequency is not the same as shown in the figure below which plots the switching energies of powerswitch $T_1$ of the FC and the NPC topologies. These differences could lead to different losses as it will be shown in the next section.
+
+![switching energies](fig/switching_energy_comparison.png)
+
+
+### Powerswitch losses and junction temperatures
+
+The figure below shows the conduction and switching losses and the junction temperatures of the main powerswitches for each topology: NPC, T-type and FC.
