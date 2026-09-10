033d13acfc4cb31dc201f1c831b5e5f061533d7f
Guillaume Fontes <guillaume.fontes@laplace.univ-tlse.fr>
2026-09-10 21:29:11 +0200

    solve issue with DPWM with addition of 1st order filter on the DQ currents

--- 44. Modulation Strategies Motor Drive/modulation_strategies_motor_drive.py
+++ 44. Modulation Strategies Motor Drive/modulation_strategies_motor_drive.py
@@ -1,3 +1,4 @@
+import json
 import os, multiprocessing
 from tqdm import tqdm
 from aesim.simba import ProjectRepository, License
@@ -11,11 +12,25 @@ from datetime import datetime
 #############################
 number_of_parallel_simulations = License.NumberOfAvailableParallelSimulationLicense() # Number of available Parallel Simulation License
 
+Npp = 4 # Number of pole pairs of the motor
+
 # Define two operating points
-rpm_speed_refs = [2000, 4000]
-torque_refs = [250, 20]
-id_refs = [-157.47, -19.32]
-iq_refs = [157.03, 11.52]
+rpm_speed_refs = {'B': 2000, 'D': 4000}
+torque_refs = {'B': 250, 'D': 20}
+id_refs = {'B': -157.47, 'D': -19.32}
+iq_refs = {'B': 157.03, 'D': 11.52}
+
+
+refs = {
+    "rpm_speed_refs": rpm_speed_refs,
+    "torque_refs": torque_refs,
+    "id_refs": id_refs,
+    "iq_refs": iq_refs,
+}
+script_folder = os.path.realpath(os.path.dirname(__file__))
+with open(os.path.join(script_folder, "operating_points.json"), "w") as f:
+    json.dump(refs, f, indent=4)
+
 
 # Define modulation strategies and switching frequencies
 modulations = ['SVPWM', 'DPWM', 'SPWM']
@@ -29,7 +44,7 @@ if os.environ.get("SIMBA_SCRIPT_TEST"): # To accelerate unit tests
 #           METHODS         #
 #############################
 
-def run_simulation(id_ref, iq_ref, rpm_speed_ref, fpwm, modulation_key, sim_number, manager_result_dict, lock):
+def run_simulation(op_point_key, fpwm, modulation_key, sim_number, manager_result_dict, lock):
     """
     Run SIMBA Simulation and place the results in "manager_result_dict"
     """ 
@@ -50,11 +65,11 @@ def run_simulation(id_ref, iq_ref, rpm_speed_ref, fpwm, modulation_key, sim_numb
     
     # Set speeds and id, iq references
     speed_source_rads = design.Circuit.GetDeviceByName('W1')
-    speed_source_rads.Voltage = float(rpm_speed_ref*np.pi/30)
+    speed_source_rads.Voltage = float(rpm_speed_refs[op_point_key]*np.pi/30)
     id_ref_block = design.Circuit.GetDeviceByName('Id_ref')
-    id_ref_block.Value = id_ref
+    id_ref_block.Value = id_refs[op_point_key]
     iq_ref_block = design.Circuit.GetDeviceByName('Iq_ref')
-    iq_ref_block.Value = iq_ref
+    iq_ref_block.Value = iq_refs[op_point_key]
     carrier = design.Circuit.GetDeviceByName('Carrier')
     carrier.Frequency = fpwm
 
@@ -64,7 +79,7 @@ def run_simulation(id_ref, iq_ref, rpm_speed_ref, fpwm, modulation_key, sim_numb
     if str(status) != "OK" or log: 
         print (job.Summary())
         return
-    
+
     # Get inverter Losses
     conduction_losses = sum([job.GetSignalByName('T' + str(n) + ' - Average Conduction Losses (W)').DataPoints[-1] for n in range(1, 7)])
     switching_losses = sum([job.GetSignalByName('T' + str(n) + ' - Average Switching Losses (W)').DataPoints[-1] for n in range(1, 7)])
@@ -73,9 +88,14 @@ def run_simulation(id_ref, iq_ref, rpm_speed_ref, fpwm, modulation_key, sim_numb
     copper_losses = job.GetSignalByName('JmagRTMotor - Copper Loss (average)').DataPoints[-1] 
     iron_losses = job.GetSignalByName('JmagRTMotor - Iron Loss (average)').DataPoints[-1] 
     if log:
-        print(f"Check speed = {rpm_speed_ref} rpm for {modulation_key} - {round(fpwm/1000)} kHz", conduction_losses, switching_losses, copper_losses, iron_losses)
+        # Get Torque
+        end_time = job.GetSignalByName('JmagRTMotor - Te').TimePoints[-1]
+        mask_time = job.GetSignalByName('JmagRTMotor - Te').TimePoints > (end_time - 60 / rpm_speed_refs[op_point_key] / Npp)
+        torque = job.GetSignalByName('JmagRTMotor - Te').DataPoints[mask_time]
+        average_torque = round(np.mean(torque) / 10) * 10
+        print(f"Check speed = {rpm_speed_refs[op_point_key]} rpm - Torque = {average_torque} Nm for {modulation_key} - {round(fpwm/1000)} kHz", conduction_losses, switching_losses, copper_losses, iron_losses)
 
-    manager_result_dict[sim_number] = [rpm_speed_ref, modulation_key, fpwm, conduction_losses, switching_losses, copper_losses, iron_losses]
+    manager_result_dict[sim_number] = [op_point_key, modulation_key, fpwm, conduction_losses, switching_losses, copper_losses, iron_losses]
 
 def run_simulation_star(args):
     """
@@ -98,10 +118,12 @@ if __name__ == "__main__": # Called only in main thread. It confirms that the co
     pool_args = []
     sim_nb = 0
     # for op_index in range(len(op_points)):
-    for id_ref, iq_ref, rpm_speed_ref in zip(id_refs, iq_refs, rpm_speed_refs):
+    for op_point_key in rpm_speed_refs.keys():
+        id_ref = id_refs[op_point_key]
+        iq_ref = iq_refs[op_point_key]
         for mod_key in modulations:
             for fpwm in switching_frequencies:
-                pool_args.append((id_ref, iq_ref, rpm_speed_ref, fpwm, mod_key, sim_nb, manager_result_dict, lock))
+                pool_args.append((op_point_key, fpwm, mod_key, sim_nb, manager_result_dict, lock))
                 sim_nb += 1
     
     # Start process pool
@@ -111,7 +133,7 @@ if __name__ == "__main__": # Called only in main thread. It confirms that the co
 
     # Store results in dedicated dictionnary
     res = dict()
-    res['speed'] = []
+    res['op_point'] = []
     res['modulation'] = []
     res['fpwm'] = []
     res['conduction'] = []
@@ -125,7 +147,6 @@ if __name__ == "__main__": # Called only in main thread. It confirms that the co
 
     # Store results in dataframe to write it in a file
     df = pd.DataFrame(res)
-    script_folder = os.path.realpath(os.path.dirname(__file__))
     filename = "mod_strat_motor_drive_" + datetime.now().strftime("%Y-%m-%d")
     df.to_pickle(os.path.join(script_folder, filename + ".pkl"))
 
--- 44. Modulation Strategies Motor Drive/modulation_strategies_motor_drive_plot.py
+++ 44. Modulation Strategies Motor Drive/modulation_strategies_motor_drive_plot.py
@@ -1,4 +1,5 @@
 #%% Load modules
+import json
 import os
 import matplotlib.pyplot as plt
 import numpy as np
@@ -10,11 +11,19 @@ import pandas as pd
 print("Load dataframe")
 
 script_folder = os.path.realpath(os.path.dirname(__file__))
-filename = "mod_strat_motor_drive_2024-04-11"
+filename = "mod_strat_motor_drive_2026-09-10"
 df = pd.read_pickle(os.path.join(script_folder, filename + ".pkl" ))
 
-torque_refs = [250, 20]
-speed_refs = set(df['speed'])
+with open(os.path.join(script_folder, "operating_points.json"), "r") as f:
+    refs = json.load(f)
+
+rpm_speed_refs, torque_refs, id_refs, iq_refs = (
+    refs["rpm_speed_refs"],
+    refs["torque_refs"],
+    refs["id_refs"],
+    refs["iq_refs"],
+)
+op_points = set(df['op_point'])
 modulations = set(df['modulation'])
 switching_frequencies = set(df['fpwm'])
 
@@ -41,34 +50,34 @@ to have an equal spacing between the bar and the left margin as between the bars
 left_margin = 0 - width/2 - (1-width)
 right_margin = len(loss_types) - 1 + width/2 + (1-width)
 
-fig, axs = plt.subplots(nrows=len(modulations), ncols=len(speed_refs), layout='constrained')
+fig, axs = plt.subplots(nrows=len(modulations), ncols=len(op_points), layout='constrained')
 fig.set_size_inches(16, 8)
 
-for op_index, speed_key in enumerate(speed_refs):
+for op_point_index, op_point_key in enumerate(op_points):
     for mod_item_index, mod_key in enumerate(modulations):
         # color constrast for graph construction
         green_color = 0.8   # initial bar color
         multiplier = 0      # shift x position
         ymax = 0            # Y-axis data values
         for fpwm in switching_frequencies:
-            df_mask_index = (df['speed']==speed_key) & (df['modulation']==mod_key) & (df['fpwm']==fpwm)
+            df_mask_index = (df['op_point']==op_point_key) & (df['modulation']==mod_key) & (df['fpwm']==fpwm)
             loss_plot = df.loc[df_mask_index, ['conduction', 'switching', 'copper', 'iron']].values.squeeze()
             ymax_ = np.max(loss_plot)
             ymax = np.max((ymax_, ymax))
             offset = width * multiplier
-            bar_plot = axs[mod_item_index, op_index].bar(x_bars + offset, loss_plot, width, label=f"{round(fpwm/1000)} kHz", align='center', color=(0, green_color , 0))
-            axs[mod_item_index, op_index].bar_label(bar_plot, padding=3, fontsize=5)
+            bar_plot = axs[mod_item_index, op_point_index].bar(x_bars + offset, loss_plot, width, label=f"{round(fpwm/1000)} kHz", align='center', color=(0, green_color , 0))
+            axs[mod_item_index, op_point_index].bar_label(bar_plot, padding=3, fontsize=5)
             multiplier += 1
             green_color = green_color - 0.3
 
         # Add some text for labels, title and custom x-axis tick labels, etc.
-        axs[mod_item_index, op_index].set_xlabel('Loss Types', fontsize=8)
-        axs[mod_item_index, op_index].set_ylabel('Losses (W)', fontsize=8)
-        axs[mod_item_index, op_index].set_title(f'Loss comparison at T = {torque_refs[op_index]} N.m & N = {speed_key} rpm for {mod_key}', fontsize=10)
-        axs[mod_item_index, op_index].set_xticks(x_bars + width*(num_bars-1)/2, loss_types)
-        axs[mod_item_index, op_index].legend(loc='upper right', ncols=num_bars)
-        axs[mod_item_index, op_index].set_ylim(0, ymax*1.25)
-        axs[mod_item_index, op_index].grid(visible=True, which="major", axis='y')
+        axs[mod_item_index, op_point_index].set_xlabel('Loss Types', fontsize=8)
+        axs[mod_item_index, op_point_index].set_ylabel('Losses (W)', fontsize=8)
+        axs[mod_item_index, op_point_index].set_title(f'Loss comparison at T = {torque_refs[op_point_key]} N.m & N = {rpm_speed_refs[op_point_key]} rpm for {mod_key}', fontsize=10)
+        axs[mod_item_index, op_point_index].set_xticks(x_bars + width*(num_bars-1)/2, loss_types)
+        axs[mod_item_index, op_point_index].legend(loc='upper right', ncols=num_bars)
+        axs[mod_item_index, op_point_index].set_ylim(0, ymax*1.25)
+        axs[mod_item_index, op_point_index].grid(visible=True, which="major", axis='y')
 
 fig.tight_layout(pad = 2)
 
@@ -79,33 +88,34 @@ plt.show()
 ########################
 print ("Plot histogram")
 
-# Identify the unique combinations of 'speed' and 'modulation'
-combinations = df[['speed', 'modulation']].drop_duplicates()
+# Identify the unique combinations of 'op_point' and 'modulation'
+combinations = df[['op_point', 'modulation']].drop_duplicates()
 
 # Calculate the number of subplots needed
 n_combinations = len(combinations)
-n_cols = len(speed_refs)  # Number of columns in the subplot grid
+n_cols = len(op_points)  # Number of columns in the subplot grid
 n_rows = n_combinations // n_cols + (n_combinations % n_cols > 0)  # Calculate rows needed
 
 # plt.figure(figsize=(15, n_rows * n_combinations))
 hist_fig, axs = plt.subplots(nrows=n_rows, ncols=n_cols, layout='constrained')
 hist_fig.set_size_inches(16, 16)
 
-# Create a subplot for each combination of 'speed' and 'modulation'
+# Create a subplot for each combination of 'op_point' and 'modulation'
 for index, (_, row) in enumerate(combinations.iterrows()):
     # plt.subplot(n_rows, n_cols, 2 * index - 1)
-    op_index = 0 if row['speed'] == list(speed_refs)[0] else 1
-    ax = axs[index % (n_cols+1), op_index]
+    op_point_col = list(op_points).index(row['op_point'])
+    mod_row = list(modulations).index(row['modulation'])
+    ax = axs[mod_row, op_point_col]
 
-    # Filter the df for the current combination of speed and modulation
-    filtered_data = df[(df['speed'] == row['speed']) & (df['modulation'] == row['modulation'])]
+    # Filter the df for the current combination of op_point and modulation
+    filtered_data = df[(df['op_point'] == row['op_point']) & (df['modulation'] == row['modulation'])]
     
     # Plot the histogram of losses for the current combination
     for loss_type in ['conduction', 'switching', 'copper', 'iron']:
         ax.hist(filtered_data['fpwm'], weights=filtered_data[loss_type], bins=20, alpha=0.5, label=loss_type)
 
     #ax.set_title(f"Speed {row['speed']} rpm, Modulation: {row['modulation']}", fontsize=11)
-    ax.set_title(f"T = {torque_refs[op_index]} N.m & N = {row['speed']} rpm for {row['modulation']}", fontsize=11)
+    ax.set_title(f"T = {torque_refs[row['op_point']]} N.m & N = {rpm_speed_refs[row['op_point']]} rpm for {row['modulation']}", fontsize=11)
     ax.set_xlabel('Switching frequency (Hz)')
     ax.set_ylabel('Losses (W)', fontsize=8)
     ax.legend()
